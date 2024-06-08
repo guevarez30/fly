cbde9ec30a64dc56a172a82d07fa7370c514a752
Taylor Guevarez <taylor@aerialapplications.com>
2024-06-08 14:03:36 -0400

    adds booking form

--- views/ui/components/popup.jsx
+++ views/ui/components/popup.jsx
@@ -3,14 +3,27 @@ function Form () {
     <>
       <h2 className='text-2xl font-bold mb-4'>Booking Information</h2>
       <form action='#' method='POST'>
+
 		<label className="input input-bordered flex items-center gap-2 mb-2">
-		  Name
-		  <input type="text" className="grow" />
+  		  <i className="fas fa-user w-4 h-4 opacity-70"></i>
+		  <input type="text" className="grow" placeholder="Name" />
 		</label>
+
+  		<label className="input input-bordered flex items-center gap-2 mb-2">
+  		  <i className="fas fa-phone w-4 h-4 opacity-70"></i>
+  		  <input id="phone" type="tel" className="grow" placeholder="Phone" />
+  		</label>
+
 		<label className="input input-bordered flex items-center gap-2 mb-2">
-		  Email
-		  <input type="text" className="grow" />
+  		  <i className="fas fa-envelope w-4 h-4 opacity-70"></i>
+		  <input type="text" className="grow" placeholder="Email" />
 		</label>
+
+		<input id="date" datetimepicker className="w-full input input-bordered flex items-center gap-2 mb-2" type="text" placeholder="Please select a date" />
+
+	  	<script>
+	  	  	flatpickr("#date", {});
+	  	</script>
       </form>
     </>
   )
@@ -19,7 +32,6 @@ function Form () {
 export function BookingForm () {
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75 z-50'>
-
 		<div className="card w-96 bg-base-100 shadow-xl">
 		  <div className="card-body">
 		      <Form />
@@ -27,7 +39,7 @@ export function BookingForm () {
 
         	<div className='flex gap-10 justify-center mb-2'>
         	  <button
-        	    className='btn btn-secondary'
+        	    className='btn'
         	    type='button'
         	    hx-trigger='click'
         	    hx-get='/noop'
@@ -47,9 +59,7 @@ export function BookingForm () {
         	  >
         	    Submit
         	  </button>
-
 			</div>
-
         </div>
       </div>
   )
--- views/ui/layout.jsx
+++ views/ui/layout.jsx
@@ -4,15 +4,15 @@ export default function Layout ({ children }) {
       <head>
         <title>App</title>
         <link href='/public/style.css' rel='stylesheet' />
-        <script src='https://unpkg.com/htmx.org@1.9.9' />
 	    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css"/>
         <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css' />
+      	<script src='https://unpkg.com/htmx.org@1.9.9' />
+	    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
       </head>
       <body id='page' >
         <div id='popup' />
         {children}
       </body>
-	  <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
     </html>
   )
 }
