7d912f4dc9ede710594aa6c3dbd2e6919b4bff2a
Taylor Guevarez <taylor@aerialapplications.com>
2024-06-08 12:00:57 -0400

    change port

--- index.jsx
+++ index.jsx
@@ -6,7 +6,7 @@ import Page from "./views/ui/page"
 
 import {BookingForm} from "./views/ui/components/popup"
 
-const port = process.env.port ?? 3000
+const port = process.env.PORT ?? 3000
 
 try{
 
